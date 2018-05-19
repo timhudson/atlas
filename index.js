f2b302a441e33930b0347f9c96f97c9a822ef585
Tim Hudson <tim.hudson15@gmail.com>
2018-05-19 16:22:04 -0400

    Switch underlying server to graphql-yoga

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const { existsSync } = require('fs')
-const url = require('url')
-const path = require('path')
-const micro = require('micro')
 const globby = require('globby')
-const { microGraphql, microGraphiql } = require('graphql-server-micro')
+const { GraphQLServer } = require('graphql-yoga');
 const parser = require('./lib/parser')
 
 module.exports = (options) => {
@@ -11,55 +8,25 @@ module.exports = (options) => {
 }
 
 class Server {
-  constructor ({ dir, graphiql = true }) {
+  constructor ({ dir }) {
     this.dir = dir
-    this.graphiql = graphiql
-    this.getContext = loadModule(`${this.dir}/_context.js`)
-    this.getScalars = loadModule(`${this.dir}/_scalars.js`)
-  }
-
-  async start (port) {
-    this.http = micro(await this.getRequestHandler())
-    return new Promise((resolve, reject) => {
-      this.http.on('error', reject)
-      this.http.on('listening', () => resolve())
-      this.http.listen(port)
-    })
-  }
-
-  async getRequestHandler () {
-    const schema = await this.getSchema()
-    return async (req, res) => {
-      if (this.graphiql) {
-        const { pathname } = url.parse(req.url)
-
-        if (req.method === 'GET' && pathname === '/graphiql') {
-          return microGraphiql({})(req, res)
-        }
-      }
-
-      const context = await this.getContext(req, res)
-      return microGraphql({ schema, context })(req, res)
-    }
+    this.context = loadModule(`${this.dir}/_context.js`)
+    this.scalars = loadModule(`${this.dir}/_scalars.js`)
   }
 
   async getSchema () {
-    const scalars = await this.getScalars()
     return globby(`${this.dir}/!(_*).js`).then(paths => {
-      return parser(paths, scalars)
+      return parser(paths, this.scalars)
     })
   }
-}
 
-const loadModule = (path) => {
-  if (existsSync(path)) {
-    const module = require(path)
-    return async (...args) => {
-      return typeof module === 'function'
-        ? module(...args)
-        : module
-    }
+  async start (port) {
+    const { typeDefs, resolvers } = await this.getSchema()
+    this.server = new GraphQLServer({ typeDefs, resolvers, context: this.context })
+    return this.server.start({ port })
   }
-
-  return () => Promise.resolve()
 }
+
+const loadModule = path => existsSync(path)
+  ? require(path)
+  : undefined
